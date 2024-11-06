a21fb40463ad84dcbd61d8186e4ee58a9a77df2b
Blnkfc <pavlinus11v@gmail.com>
2024-11-06 11:46:05 +0200

    Updated manifest, started connectiong the wallet

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { TonConnectUIProvider, TonConnectButton } from '@tonconnect/ui-react'
- 
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
 function App() {
-  
+
 
   return (
-    <>
-    a
-    </>
+    <TonConnectUIProvider manifestUrl='https://raw.githubusercontent.com/Blnkfc/RealisTestTask/refs/heads/main/manifest.json' >
+
+      a
+
+    </TonConnectUIProvider>
   )
 }
 
