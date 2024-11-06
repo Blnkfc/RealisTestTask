6a8084751db49fd1654ed286d12f4f4e9f268920
Blnkfc <pavlinus11v@gmail.com>
2024-11-06 13:49:18 +0200

    Fixing connection issues on android

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,11 @@
-import { TonConnectButton, useTonConnectUI } from '@tonconnect/ui-react';
+import { TonConnectButton } from '@tonconnect/ui-react';
 
 
 export const Header = () => {
-    const [tonConnectUI, setOptions] = useTonConnectUI();
     return (
       <header>
         <span>Your wallet:</span>
-        <button onClick={() => tonConnectUI.openModal()} >
-            Connect Wallet
-        </button>
+        <TonConnectButton />
       </header>
     );
   };
